b926a6d770e9c72fbcf933fe9dc84ec33eac7e1b
Bas Terwijn <B.Terwijn@uva.nl>
2012-07-16 19:10:14 +0200

    added HumanTracker messages

---
+++ UvA/HumanTracker/src/Tracker.cpp
@@ -0,0 +1,17 @@
+
+#include <ros/ros.h>
+
+int main(int argc,char **argv)
+{
+  ros::init(argc, argv, "Tracker");
+
+  // read human locations
+
+  // read human indentities
+
+  // do tracking
+
+  // publish tracked humans
+
+  return 0;
+}
