27614d1b495edb62e389dfab37c80665563b0459
Bas Terwijn <B.Terwijn@uva.nl>
2012-07-16 19:21:24 +0200

    minor changes

--- UvA/StaticCameraLocalisation/src/Localisation.cpp
+++ UvA/StaticCameraLocalisation/src/Localisation.cpp
@@ -5,5 +5,22 @@ int main(int argc,char **argv)
 {
   ros::init(argc, argv, "Localisation");
 
+  // read files
+
+  ros::Rate loop_rate(10);
+  while(ros::ok())
+  {
+    // read image from camera
+
+    // process image
+
+    // publish human locations
+    
+    // publish samples particles
+    
+    ros::spinOnce();
+    loop_rate.sleep();
+  }
+
   return 0;
 }
