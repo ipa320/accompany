1aebb59c2f54049a53d821d1252f487ea04e1584
Bas Terwijn <B.Terwijn@uva.nl>
2012-08-07 13:42:28 +0200

    building with TimTracker and OpenCV

--- accompany_human_tracker/src/human_tracker.cpp
+++ accompany_human_tracker/src/human_tracker.cpp
@@ -4,6 +4,7 @@
 #include <accompany_human_tracker/TrackedHumans.h>
 
 #include <MyTracker.h>
+#include <TimTracker/Tracker.h>
 
 #include <iostream>
 using namespace std;
@@ -11,6 +12,9 @@ using namespace std;
 // globals
 ros::Publisher trackedHumansPub;
 MyTracker myTracker;
+
+Tracker tracker;
+ros::Time prevNow;
 
 void humanLocationsReceived(const accompany_human_tracker::HumanLocations::ConstPtr& humanLocations)
 {
@@ -21,13 +25,34 @@ void humanLocationsReceived(const accompany_human_tracker::HumanLocations::Const
     cout<<"humanLocations["<<i<<"].z="<<humanLocations->locations[i].z<<endl;
   }
   */
-  myTracker.trackHumans(humanLocations);
-  trackedHumansPub.publish(myTracker.getTrackedHumans());
+
+  // using simple MyTracker
+  //myTracker.trackHumans(humanLocations);
+  //trackedHumansPub.publish(myTracker.getTrackedHumans());
+
+  // using TimTracker
+  vector<Tracker::TrackPoint> trackPoints;
+  for (unsigned int i=0;i<humanLocations->locations.size();i++)
+  {
+    Tracker::TrackPoint point = {humanLocations->locations[i].x,
+                                 humanLocations->locations[i].y,
+                                 0.01,
+                                 0.01};
+    trackPoints.push_back(point);
+  }
+  double deltaTime=0;
+  ros::Time now=ros::Time::now();
+  ros::Duration duration=now-prevNow;
+  deltaTime=duration.toSec();
+  prevNow=now;
+  tracker.update(trackPoints, deltaTime);
+
 }
 
 int main(int argc,char **argv)
 {
   ros::init(argc, argv, "HumanTracker");
+  prevNow=ros::Time::now();
 
   // create publisher and subscribers
   ros::NodeHandle n;
