445f4a5c7f6fc1568be10d0bd1f377fa8121f6cf
Bas Terwijn <B.Terwijn@uva.nl>
2012-08-15 21:18:46 +0200

    subscribing to PeopleDetectionArray and labeling tracks

--- accompany_human_tracker/src/human_tracker.cpp
+++ accompany_human_tracker/src/human_tracker.cpp
@@ -2,10 +2,14 @@
 #include <ros/ros.h>
 #include <accompany_human_tracker/HumanLocations.h>
 #include <accompany_human_tracker/TrackedHumans.h>
+#include <cob_people_detection_msgs/PeopleDetection.h>
+#include <cob_people_detection_msgs/PeopleDetectionArray.h>
+#include <tf/transform_listener.h>
 
 #include <MyTracker.h>
 #include <TimTracker/Tracker.h>
 
+#include <limits>
 #include <iostream>
 using namespace std;
 
@@ -18,17 +22,19 @@ using namespace std;
 // globals
 ros::Publisher trackedHumansPub;
 ros::Time prevNow;
+tf::TransformListener *listener=NULL;
 
+accompany_human_tracker::TrackedHumans trackedHumans;// array of tracked humans
 #if TRACKER == MY_TRACKER
 MyTracker myTracker;
 #elif TRACKER == TIM_TRACKER
 Tracker tracker;
 #endif
 
+
+// receive human locations and track them
 void humanLocationsReceived(const accompany_human_tracker::HumanLocations::ConstPtr& humanLocations)
 {
-accompany_human_tracker::TrackedHumans trackedHumans;
-
 #if TRACKER == MY_TRACKER // using simple MyTracker
  
   myTracker.trackHumans(humanLocations);
@@ -63,9 +69,102 @@ accompany_human_tracker::TrackedHumans trackedHumans;
     trackedHuman.id=it->id;
     trackedHumans.trackedHumans.push_back(trackedHuman);
   }
-
 #endif
-trackedHumansPub.publish(trackedHumans);
+
+  trackedHumansPub.publish(trackedHumans);
+}
+
+
+class MakeMatch
+{
+public:
+
+  MakeMatch()
+  {
+    bestSumDist=numeric_limits<double>::max();
+  }
+
+  void match(double *dist,int nrTracks,int nrIdentities,vector<int> *assigned,double sumDist)
+  {
+    if (assigned->size()==(unsigned)nrIdentities)
+    {
+      if (sumDist<bestSumDist)
+      {
+        bestSumDist=sumDist;
+        bestAssigned=*assigned;
+      }
+    }
+    else
+    {
+      int iden=assigned->size();// identity to assign
+      for (int i=0;i<nrTracks;i++)
+      {
+        vector<int> newAssigned=*assigned;
+        newAssigned.push_back(i);
+        double newSumDist=sumDist+dist[i*nrIdentities+iden];
+        match(dist,nrTracks,nrIdentities,&newAssigned,newSumDist);
+      }
+    }
+  }
+
+  void match(double *dist,int nrTracks,int nrIdentities)
+  {
+    vector<int> assigned;
+    match(dist,nrTracks,nrIdentities,&assigned,0);
+  }
+
+  vector<int> *getBestAssigned()
+  {
+    return &bestAssigned;
+  }
+
+  vector<int> bestAssigned;
+  double bestSumDist;
+};
+
+// compute the distance matrix of every track to every identified human
+void match(cob_people_detection_msgs::PeopleDetectionArray &transformedIdentifiedHumans)
+{
+  unsigned int nrTracks=trackedHumans.trackedHumans.size();
+  unsigned int nrIdentities=transformedIdentifiedHumans.detections.size();
+  double dist[nrTracks*nrIdentities];
+  for (unsigned int i=0;i<nrTracks;i++)
+  {
+    accompany_human_tracker::TrackedHuman trackedHuman=trackedHumans.trackedHumans[i];
+    for (unsigned int j=0;j<nrIdentities;j++)
+    {
+      cob_people_detection_msgs::PeopleDetection identity=transformedIdentifiedHumans.detections[j];
+
+      double dx=trackedHuman.location.x-identity.pose.pose.position.x;
+      double dy=trackedHuman.location.y-identity.pose.pose.position.y;
+      double dz=trackedHuman.location.y-identity.pose.pose.position.z;
+      dist[i*nrIdentities+j]=dx*dx+dy*dy+dz*dz;
+    }
+  }
+  MakeMatch makeMatch;
+  makeMatch.match(dist,nrTracks,nrIdentities);
+  
+}
+
+// receive identities and transform them to the camera's coordinate system
+void identityReceived(const cob_people_detection_msgs::PeopleDetectionArray::ConstPtr& identifiedHumans)
+{
+  cob_people_detection_msgs::PeopleDetectionArray transformedIdentifiedHumans=*identifiedHumans;
+  for (unsigned int i=0;i<identifiedHumans->detections.size();i++)
+  {
+    string identity=identifiedHumans->detections[i].label;
+    const geometry_msgs::PoseStamped pose=identifiedHumans->detections[i].pose;
+
+    // transform to camera coordinate system
+    geometry_msgs::PoseStamped transPose;
+    listener->transformPose("/cam1",pose,transPose);
+    geometry_msgs::Point pos=transPose.pose.position;
+
+    cout<<"identified '"<<identity<<"' at ("<<pos.x<<","<<pos.y<<","<<pos.z<<")"<<endl;
+    transformedIdentifiedHumans.detections[i].pose=transPose;
+  }
+  match(transformedIdentifiedHumans);
+  
 }
 
 int main(int argc,char **argv)
@@ -74,9 +173,14 @@ int main(int argc,char **argv)
 
   // create publisher and subscribers
   ros::NodeHandle n;
-  prevNow=ros::Time::now();
+
+  prevNow=ros::Time::now();  
+  tf::TransformListener initListener;
+  listener=&initListener;
+
   trackedHumansPub=n.advertise<accompany_human_tracker::TrackedHumans>("/trackedHumans",10);
   ros::Subscriber humanLocationsSub=n.subscribe<accompany_human_tracker::HumanLocations>("/humanLocations",10,humanLocationsReceived);
+  ros::Subscriber identitySub=n.subscribe<cob_people_detection_msgs::PeopleDetectionArray>("/identity",10,identityReceived);
   ros::spin();
 
   return 0;
