f077cd06337bf11d3d078ba81ae2815408f585f3
Bas Terwijn <B.Terwijn@uva.nl>
2012-08-30 21:18:12 +0200

    adding accompany_uva_utils for code that is reused in different packages

--- accompany_uva_utils/include/accompany_uva_utils/uva_utils.h
+++ accompany_uva_utils/include/accompany_uva_utils/uva_utils.h
@@ -1,35 +1,33 @@
 
 #include <ros/ros.h>
-#include <geometry_msgs/PoseStamped.h>
 #include <std_msgs/UInt32.h>
 
-#include <boost/archive/text_oarchive.hpp> 
-#include <boost/archive/text_iarchive.hpp> 
-
 #include <iostream>
-#include <fstream> 
-using namespace std;
+#include <fstream>
 
-template<class T> void save_msg(T &t,string filename)
+template<class T> void save_msg(T &t,std::string filename)
 {
   uint32_t size=ros::serialization::serializationLength(t);
   boost::shared_array<uint8_t> buffer(new uint8_t[size]);
   ros::serialization::OStream ostream(buffer.get(),size);
   ros::serialization::serialize(ostream,t);
-  std::ofstream file(filename.c_str(),ios::out|ios::binary);
+  std::ofstream file(filename.c_str(),std::ios::out|std::ios::binary);
   for (unsigned int i=0;i<size;i++)
     file<<buffer[i];
   file.close();
 }
 
-template<class T> void load_msg(T &t,string filename)
+template<class T> void load_msg(T &t,std::string filename)
 {
-  uint32_t size=ros::serialization::serializationLength(t);
-  int byteSize=size*sizeof(uint32_t);
-  unsigned char *buf=new unsigned char[byteSize];
-  std::ifstream file(filename.c_str(),ios::in|ios::binary);
-  file.readsome((char *)buf,byteSize);  
+  std::ifstream file(filename.c_str(),std::ios::in|std::ios::binary);
+  file.seekg (0, std::ios::end);
+  int length = file.tellg();// get length of file
+  file.seekg (0, std::ios::beg);
+  unsigned char *buf=new unsigned char[length];
+  file.readsome((char *)buf,length);// read hole file
+  file.close();
   boost::shared_array<uint8_t> buffer(buf);
-  ros::serialization::IStream istream(buffer.get(),size);
+  uint32_t size=ros::serialization::serializationLength(t);
+  ros::serialization::IStream istream(buffer.get(),length/sizeof(uint8_t));
   ros::serialization::deserialize(istream,t);
 }
