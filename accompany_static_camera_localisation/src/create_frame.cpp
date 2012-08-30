e09a7e75766b519e8cf09b670ae305d0bce34a76
Bas Terwijn <B.Terwijn@uva.nl>
2012-08-30 22:44:30 +0200

    use Transform mesage to represent coordinate frame

--- accompany_static_camera_localisation/src/create_frame.cpp
+++ accompany_static_camera_localisation/src/create_frame.cpp
@@ -1,53 +1,33 @@
 
 #include <ros/ros.h>
-#include <geometry_msgs/PoseStamped.h>
-#include <std_msgs/UInt32.h>
+#include <geometry_msgs/TransformStamped.h>
 
-#include <boost/archive/text_oarchive.hpp> 
-#include <boost/archive/text_iarchive.hpp> 
+#include <accompany_uva_utils/uva_utils.h>
 
 #include <iostream>
-#include <fstream> 
 using namespace std;
-
-template<class T> void save_msg(T &t,string filename)
-{
-  uint32_t size=ros::serialization::serializationLength(t);
-  boost::shared_array<uint8_t> buffer(new uint8_t[size]);
-  ros::serialization::OStream ostream(buffer.get(),size);
-  ros::serialization::serialize(ostream,t);
-  std::ofstream file(filename.c_str(),ios::out|ios::binary);
-  for (unsigned int i=0;i<size;i++)
-    file<<buffer[i];
-  file.close();
-}
-
-template<class T> void load_msg(T &t,string filename)
-{
-  uint32_t size=ros::serialization::serializationLength(t);
-  int byteSize=size*sizeof(uint32_t);
-  unsigned char *buf=new unsigned char[byteSize];
-  std::ifstream file(filename.c_str(),ios::in|ios::binary);
-  file.readsome((char *)buf,byteSize);
-  
-  boost::shared_array<uint8_t> buffer(buf);
-  ros::serialization::IStream istream(buffer.get(),size);
-  ros::serialization::deserialize(istream,t);
-}
 
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "create_frame");
   
-  geometry_msgs::PoseStamped p;
-  p.pose.position.x=88;
-  p.pose.orientation.w=1;
-  save_msg(p,"test.dat");
-  cout<<"p:"<<p;
+  char *filename="frame.dat";
+  cout<<"create some frame and write to file '"<<filename<<"'"<<endl;
+  geometry_msgs::TransformStamped transform;
+  transform.header.frame_id="/map";
+  transform.child_frame_id="/overhead1";
+  transform.transform.translation.x=0;
+  transform.transform.translation.y=0;
+  transform.transform.translation.z=0;
+  transform.transform.rotation.x=0;
+  transform.transform.rotation.y=0;
+  transform.transform.rotation.z=0;
+  transform.transform.rotation.w=1;
+  save_msg(transform,filename); // write to file
 
-  geometry_msgs::PoseStamped p2;
-  load_msg(p2,"test.dat");
-  cout<<"p2:"<<p2;
-
+  cout<<"read from file '"<<filename<<"' and print, just a test:"<<endl;
+  geometry_msgs::TransformStamped transform2;
+  load_msg(transform2,filename);
+  cout<<transform2;
   
 }
