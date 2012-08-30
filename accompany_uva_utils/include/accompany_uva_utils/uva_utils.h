f4291399f4e6a94cc08330af47559b01d0c17053
Bas Terwijn <B.Terwijn@uva.nl>
2012-08-30 21:17:15 +0200

    adding accompany_uva_utils for code that is reused in different packages

---
+++ accompany_uva_utils/include/accompany_uva_utils/uva_utils.h
@@ -0,0 +1,35 @@
+
+#include <ros/ros.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <std_msgs/UInt32.h>
+
+#include <boost/archive/text_oarchive.hpp> 
+#include <boost/archive/text_iarchive.hpp> 
+
+#include <iostream>
+#include <fstream> 
+using namespace std;
+
+template<class T> void save_msg(T &t,string filename)
+{
+  uint32_t size=ros::serialization::serializationLength(t);
+  boost::shared_array<uint8_t> buffer(new uint8_t[size]);
+  ros::serialization::OStream ostream(buffer.get(),size);
+  ros::serialization::serialize(ostream,t);
+  std::ofstream file(filename.c_str(),ios::out|ios::binary);
+  for (unsigned int i=0;i<size;i++)
+    file<<buffer[i];
+  file.close();
+}
+
+template<class T> void load_msg(T &t,string filename)
+{
+  uint32_t size=ros::serialization::serializationLength(t);
+  int byteSize=size*sizeof(uint32_t);
+  unsigned char *buf=new unsigned char[byteSize];
+  std::ifstream file(filename.c_str(),ios::in|ios::binary);
+  file.readsome((char *)buf,byteSize);  
+  boost::shared_array<uint8_t> buffer(buf);
+  ros::serialization::IStream istream(buffer.get(),size);
+  ros::serialization::deserialize(istream,t);
+}
