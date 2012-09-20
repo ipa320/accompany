8327b774b72b5c4902738ccea3e015885ed5df3f
camera2 <camera2@camera2-PC.(none)>
2012-09-20 16:54:02 +0100

    added running_samples

--- UvA/installUvA.sh
+++ UvA/installUvA.sh
@@ -5,7 +5,7 @@ echo "install vxl-1.17.0"
 
 sudo apt-get -y install ubuntu-restricted-extras
 
-sudo apt-get -y install aptitude emacs git libopencv2.3-dev cmake libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev gstreamer-tools gstreamer0.10-x git gitk
+sudo apt-get -y install aptitude emacs git libopencv2.3-dev cmake libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev gstreamer-tools gstreamer0.10-x ros-electric-openni-kinect
 
 cd
 mkdir -p ros
@@ -46,6 +46,11 @@ sudo make install
 cd ~/ros/accompany/UvA/dependencies/gscam
 rosdep install gscam
 rosmake
+
+# skeleton_marker
+svn checkout http://pi-robot-ros-pkg.googlecode.com/svn/trunk/skeleton_markers
+rosdep install skeleton_markers
+rosmake skeleton_markers
 
 # cob_perception_common
 cd ~/ros
