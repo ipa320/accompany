ef4ca1432dac23becf9992c8a0d130a6bfeac19e
Ninghang Hu <n.hu@uva.nl>
2012-09-25 09:56:37 +0100

    use 1/4 resolution to view images, save images using theora codec

---
+++ accompany_static_camera_localisation/scripts/recordImages.sh
@@ -0,0 +1,6 @@
+rosbag record -b 0 /gscam/image_raw/theora
+#
+# to play back use image_transport to convert from theora to raw images like so:
+#
+# rosrun image_transport republish theora in:=/gscam/image_raw _image_transport:=theora raw out:=/gscam/image_raw
+#
