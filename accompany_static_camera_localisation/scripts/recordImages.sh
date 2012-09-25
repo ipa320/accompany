354b8c1614ef1cce06e087cd188080173211a6a4
Ninghang Hu <n.hu@uva.nl>
2012-09-25 13:10:41 +0100

    added rosbag playback script

--- accompany_static_camera_localisation/scripts/recordImages.sh
+++ accompany_static_camera_localisation/scripts/recordImages.sh
@@ -3,7 +3,7 @@ rosbag record -b 0 /gscam/image_raw/theora
 # to play back use image_transport to convert from theora to raw images like so:
 #
 #
-# rosbag play -l <bag-file>
+# rosbag play -l -d 5 <bag-file>
 # rosrun image_transport republish theora in:=/gscam/image_raw _image_transport:=theora raw out:=/gscam/image_raw
-#
+# rosrun image_view image_view image:=/gscam/image_raw
 #
