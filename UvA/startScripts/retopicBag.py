b291ac5b4e8c3313da76c63b804d4e1ea2d4400e
Bas Terwijn <B.Terwijn@uva.nl>
2012-10-23 23:42:41 +0200

    now viewing robotHouse data camera1 and camera2 in sync

--- UvA/startScripts/retopicBag.py
+++ UvA/startScripts/retopicBag.py
@@ -11,4 +11,4 @@ topicPrefix=sys.argv[3]
 
 with rosbag.Bag(outputBag, 'w') as outbag:
     for topic, msg, t in rosbag.Bag(inputBag).read_messages():
-        outbag.write(topic + topicPrefix, msg, t)
+        outbag.write(topicPrefix + topic, msg, t)
