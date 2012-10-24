695ad7ab2c8e7c9e073f472720dad4c39cc8901a
Bas Terwijn <B.Terwijn@uva.nl>
2012-10-24 18:23:01 +0200

    image_saver save number of consecutive images to a path and saves the background_list.txt with the list of images

--- accompany_static_camera_localisation/src/image_saver.cpp
+++ accompany_static_camera_localisation/src/image_saver.cpp
@@ -1,33 +1,41 @@
 #include <opencv/cv.h>
 #include <opencv/highgui.h>
 
+#include <iostream>
+#include <fstream>
+
 #include <ros/ros.h>
 #include <cv_bridge/CvBridge.h>
 #include <image_transport/image_transport.h>
 //#include <camera_calibration_parsers/parse.h>
+
 #include <boost/format.hpp>
+#include <boost/program_options.hpp>
+
+namespace po = boost::program_options;
+using namespace std;
 
 sensor_msgs::CvBridge g_bridge;
-int g_count = 0;
 boost::format g_format;
+int g_count = 0;
+int nrImage;
+string path;
 
-void callback(const sensor_msgs::ImageConstPtr& image,
-    const sensor_msgs::CameraInfoConstPtr& info)
+ofstream backgroundList;
+
+void callback(const sensor_msgs::ImageConstPtr& image)
 {
   if (g_bridge.fromImage(*image, "bgr8"))
   {
     IplImage *image = g_bridge.toIpl();
     if (image)
     { 
-      if ((g_count % 10) == 0)
-      {
-        std::string filename = (g_format % g_count % "jpg").str();
-        cvSaveImage(filename.c_str(), image);
+        string filename = (g_format % g_count % "jpg").str();
+        cvSaveImage((path+"/"+filename).c_str(), image);
+	backgroundList<<(path+"/"+filename).c_str()<<endl;
         ROS_INFO("Saved image %s", filename.c_str());
-      //      filename = (g_format % g_count % "ini").str();
-      //      camera_calibration_parsers::writeCalibration(filename, "camera", *info);
-      }
-      g_count++;
+	if (++g_count>nrImage-1)
+	  ros::shutdown();
     }
     else
     {
@@ -36,17 +44,57 @@ void callback(const sensor_msgs::ImageConstPtr& image,
   }
   else
     ROS_ERROR("Unable to convert %s image to bgr8", image->encoding.c_str());
-}
+} 
 
 int main(int argc, char** argv)
 {
+  // handling arguments
+  po::options_description optionsDescription(
+      "Image saver\n"
+      "Available remappings:\n"
+      "  image:=<image-topic>\n"
+      "\n"
+      "Allowed options");
+  optionsDescription.add_options()
+    ("help,h", "produce help message")
+    ("nr,n", po::value<int>(&nrImage)->default_value(10),"number of images to save")
+    ("path,p", po::value<string>(&path)->default_value("./"),"path to save images to");
+  po::variables_map variablesMap;
+
+  try
+  {
+    po::store(po::parse_command_line(argc, argv, optionsDescription),variablesMap);
+    po::notify(variablesMap);
+  }
+  catch (const exception& e)
+  {
+    cerr << "--------------------" << endl;
+    cerr << "- " << e.what() << endl;
+    cerr << "--------------------" << endl;
+    cerr << optionsDescription << endl;
+    return 1;
+  }
+  if (variablesMap.count("help")) 
+  {
+    cout<<optionsDescription<<endl;
+    return 1;
+  }
+
+  cout<<"number of images to save: "<<nrImage<<endl;
+  cout<<"path to save images to: '"<<path<<"'"<<endl;
+
+  backgroundList.open((path+"/"+"background_list.txt").c_str());
+
   ros::init(argc, argv, "image_saver", ros::init_options::AnonymousName);
   ros::NodeHandle nh;
+  string resolved_image=nh.resolveName("image");
+  cout<<"subscribe to image topic: "<<resolved_image<<endl;
+
   g_format.parse("%04i.%s");
-  image_transport::ImageTransport it(nh);
-  std::string topic = nh.resolveName("image");
-  image_transport::CameraSubscriber sub = it.subscribeCamera(topic, 1,
-      &callback);
+  image_transport::ImageTransport it(nh);  
+  image_transport::Subscriber sub = it.subscribe(resolved_image, 1, &callback);
 
   ros::spin();
+  backgroundList.close();
+  return 1;
 }
