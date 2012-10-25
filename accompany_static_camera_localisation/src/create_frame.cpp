44d6c2a0090826b6d19870bb02b8dda946e95622
Bas Terwijn <B.Terwijn@uva.nl>
2012-10-25 16:15:57 +0200

    create_frame now can create named coordinate frame with translation and rotation in xy plane

--- accompany_static_camera_localisation/src/create_frame.cpp
+++ accompany_static_camera_localisation/src/create_frame.cpp
@@ -5,26 +5,62 @@
 
 #include <accompany_uva_utils/uva_utils.h>
 
+#include <boost/program_options.hpp>
 #include <iostream>
 using namespace std;
+using namespace boost::program_options;
 
 int main(int argc, char **argv)
 {
+  string parent;
+  string frameName;
+  double a;
+  double x,y,z;
+
+  // handling arguments
+  options_description optionsDescription(
+      "Create a coordinate frame relative to a parent\n"
+      "Allowed options");
+  optionsDescription.add_options()
+    ("frame-name,f", value<string>(&frameName)->required(),"name of the new frame")
+    ("parent,p", value<string>(&parent)->default_value("/map"),"name of parent frame")
+    ("angle,a", value<double>(&a)->default_value(0.0),"angle in xy plane")
+    ("xpos,x", value<double>(&x)->default_value(0.0),"x position")
+    ("ypos,y", value<double>(&y)->default_value(0.0),"y position")
+    ("zpos,z", value<double>(&z)->default_value(0.0),"z position");
+  
+  variables_map variablesMap;
+
+  try
+  {
+    store(parse_command_line(argc, argv, optionsDescription),
+        variablesMap);
+    notify(variablesMap);
+  }
+  catch (const std::exception& e)
+  {
+    std::cout << "--------------------" << std::endl;
+    std::cerr << "- " << e.what() << std::endl;
+    std::cout << "--------------------" << std::endl;
+    std::cout << optionsDescription << std::endl;
+    return 1;
+  }
+
+
   ros::init(argc, argv, "create_frame");
   
   string filename="frame.dat";
   cout<<"create some frame and write to file '"<<filename<<"'"<<endl;
   geometry_msgs::TransformStamped transformStamped;
 
-  double a=0;
   tf::Transform transform=tf::Transform(btMatrix3x3( cos(a),sin(a),0, // rotation matrix
                                                     -sin(a),cos(a),0,
                                                     0,0,1),  
-                                        btVector3(0,0,0)); // translation vector
+                                        btVector3(x,y,z)); // translation vector
   tf::StampedTransform stampedTransform=tf::StampedTransform(transform,     // the transform
                                                              ros::Time(0),  // time, not used here
-                                                             "/map",        // parent coordinate frame
-                                                             "/overhead1"); // child coordinate frame
+                                                             parent,        // parent coordinate frame
+                                                             frameName); // child coordinate frame
   tf::transformStampedTFToMsg(stampedTransform,transformStamped);
   save_msg(transformStamped,filename); // write to file
  
