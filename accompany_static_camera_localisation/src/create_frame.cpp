aea16fcc898e06d02d1e0ff9e177d5e0b9e574f9
Bas Terwijn <B.Terwijn@uva.nl>
2012-11-06 13:04:10 +0100

    allow create_frame to also only view/print an existing coordinate frame file instead of overwritting it

--- accompany_static_camera_localisation/src/create_frame.cpp
+++ accompany_static_camera_localisation/src/create_frame.cpp
@@ -14,21 +14,24 @@ int main(int argc, char **argv)
 {
   string parent;
   string frameName;
+  string filename;
   double a;
   double x,y,z;
 
   // handling arguments
   options_description optionsDescription(
-      "Create a coordinate frame relative to a parent\n"
+      "Create a coordinate frame relative to a parent and save it to disk\n"
       "Allowed options");
   optionsDescription.add_options()
     ("help,h","show help message")
-    ("frame-name,f", value<string>(&frameName)->required(),"name of the new frame")
+    ("filename,f", value<string>(&filename)->default_value("frame.dat"),"filename of the coordiante name file")
+    ("frame-name,n", value<string>(&frameName)->default_value("child_frame"),"name of the new frame")
     ("parent,p", value<string>(&parent)->default_value("/map"),"name of parent frame")
     ("angle,a", value<double>(&a)->default_value(0.0),"angle in xy plane")
     ("xpos,x", value<double>(&x)->default_value(0.0),"x position")
     ("ypos,y", value<double>(&y)->default_value(0.0),"y position")
-    ("zpos,z", value<double>(&z)->default_value(0.0),"z position");
+    ("zpos,z", value<double>(&z)->default_value(0.0),"z position")
+    ("view-only,v","don't create a new coordinate frame, only print current file to screen");
   
   variables_map variablesMap;
   try
@@ -48,23 +51,26 @@ int main(int argc, char **argv)
 
 
   ros::init(argc, argv, "create_frame");
-  
-  string filename="frame.dat";
-  cout<<"create some frame and write to file '"<<filename<<"'"<<endl;
-  geometry_msgs::TransformStamped transformStamped;
 
-  tf::Transform transform=tf::Transform(btMatrix3x3( cos(a),sin(a),0, // rotation matrix
-                                                    -sin(a),cos(a),0,
-                                                    0,0,1),  
-                                        btVector3(x,y,z)); // translation vector
-  tf::StampedTransform stampedTransform=tf::StampedTransform(transform,     // the transform
-                                                             ros::Time(0),  // time, not used here
-                                                             parent,        // parent coordinate frame
-                                                             frameName); // child coordinate frame
-  tf::transformStampedTFToMsg(stampedTransform,transformStamped);
-  save_msg(transformStamped,filename); // write to file
- 
+  if (variablesMap.count("view-only")==0)
+  {
+    // create frame
+    cout<<"create some frame and write to file '"<<filename<<"'"<<endl;
+    geometry_msgs::TransformStamped transformStamped;
 
+    tf::Transform transform=tf::Transform(btMatrix3x3( cos(a),sin(a),0, // rotation matrix
+                                                       -sin(a),cos(a),0,
+                                                       0,0,1),  
+                                          btVector3(x,y,z)); // translation vector
+    tf::StampedTransform stampedTransform=tf::StampedTransform(transform,     // the transform
+                                                               ros::Time(0),  // time, not used here
+                                                               parent,        // parent coordinate frame
+                                                               frameName); // child coordinate frame
+    tf::transformStampedTFToMsg(stampedTransform,transformStamped);
+    save_msg(transformStamped,filename); // write to file
+  }
+
+  // load frame and print
   cout<<"read from file '"<<filename<<"' and print, just a test:"<<endl;
   geometry_msgs::TransformStamped transformStamped2;
   load_msg(transformStamped2,filename);
