4e1db39548222d8054b622698ee25ba52f0894a2
Patrizia Marti <patrizia@patrizia.(none)>
2013-07-04 12:52:15 +0200

    Removed old Siena GUI directories /MarcoBongini

--- Siena GUI June 2013/AccompanyService/src/AccompanyService/srv/__init__.py
+++
@@ -1,3 +0,0 @@
-from ._AccompanyAction import *
-from ._AddTwoInts import *
-from ._db_msg import *
--- Siena GUI June 2013/GUI/AccompanyProjectGUI_for_robot/src/it/unisi/accompany/speech/MicrophoneInputListener.java
+++
@@ -1,6 +0,0 @@
-package it.unisi.accompany.speech;
-
-
-public interface MicrophoneInputListener {
-  public void processAudioFrame(short[] audioFrame);
-}
