8b8093abe5a6b1de9b360c5a0a90afd2fa02167b
accompany-cob3-6 <rmb@ipa.fhg.de>
2013-10-07 15:41:06 +0200

    Removed old Scheduler again

--- COBScheduler/build/devel/setup.zsh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env zsh
-# generated from catkin/cmake/templates/setup.zsh.in
-
-CATKIN_SHELL=zsh
-emulate sh # emulate POSIX
-. "/home/accompany/git/care-o-bot/accompany/COBScheduler/build/devel/setup.sh"
-emulate zsh # back to zsh mode
