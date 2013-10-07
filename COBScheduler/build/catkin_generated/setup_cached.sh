d5b89cb17bc7079a7e5bd1b6b637e98be2575162
accompany-cob3-6 <accompany@cob3-6>
2013-10-07 13:20:04 +0200

    backup of images republisher

---
+++ COBScheduler/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CATKIN_TEST_RESULTS_DIR="/home/accompany/git/care-o-bot/accompany/COBScheduler/build/test_results"
+export CMAKE_PREFIX_PATH="/home/accompany/git/care-o-bot/accompany/COBScheduler/build/devel:$CMAKE_PREFIX_PATH"
+export CPATH="/home/accompany/git/care-o-bot/accompany/COBScheduler/build/devel/include:$CPATH"
+export LD_LIBRARY_PATH="/home/accompany/git/care-o-bot/accompany/COBScheduler/build/devel/lib:$LD_LIBRARY_PATH"
+export PATH="/home/accompany/git/care-o-bot/accompany/COBScheduler/build/devel/bin:$PATH"
+export PKG_CONFIG_PATH="/home/accompany/git/care-o-bot/accompany/COBScheduler/build/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
+export PYTHONPATH="/home/accompany/git/care-o-bot/accompany/COBScheduler/build/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export ROS_PACKAGE_PATH="/home/accompany/git/care-o-bot/accompany/COBScheduler:/home/accompany/moveit/src:/opt/ros/groovy/share:/opt/ros/groovy/stacks"
+export ROS_TEST_RESULTS_DIR="/home/accompany/git/care-o-bot/accompany/COBScheduler/build/test_results"
