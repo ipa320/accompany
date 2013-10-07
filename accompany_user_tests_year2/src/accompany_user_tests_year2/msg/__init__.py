9c7a25c05b6aabaa361dd0582f55e718759b9b6f
accompany-cob3-6 <accompany@cob3-6>
2013-10-07 16:24:19 +0200

    removed test/src/test instead to merge...

--- accompany_user_tests_year2/src/accompany_user_tests_year2/msg/__init__.py
+++
@@ -1,7 +0,0 @@
-from ._UnloadTrayAction import *
-from ._UnloadTrayFeedback import *
-from ._UnloadTrayResult import *
-from ._UnloadTrayGoal import *
-from ._UnloadTrayActionGoal import *
-from ._UnloadTrayActionResult import *
-from ._UnloadTrayActionFeedback import *
