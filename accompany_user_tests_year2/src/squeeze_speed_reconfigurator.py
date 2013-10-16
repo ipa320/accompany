74f9502871ab8ebe2447cee351e948857a1cb091
accompany-cob3-6 <accompany@cob3-6>
2013-10-16 11:02:01 +0200

    merged tray server change

--- accompany_user_tests_year2/src/squeeze_speed_reconfigurator.py
+++ accompany_user_tests_year2/src/squeeze_speed_reconfigurator.py
@@ -2,6 +2,7 @@ import roslib
 import socket
 import dynamic_reconfigure.client
 import rospy
+import MySQLdb
 
 UDP_IP = "192.168.1.142"
 UDP_PORT = 8042
@@ -9,6 +10,13 @@ UDP_PORT = 8042
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
 sock.bind((UDP_IP, UDP_PORT))
+
+db = MySQLdb.connect(host="localhost",
+					user="accompanyUser",
+					passwd="accompany",
+					db="Accompany")
+cur = db.cursor()
+
 
 rospy.init_node('squeezeme_speed_cfg', anonymous=True)
 client = dynamic_reconfigure.client.Client("move_base/DWAPlannerROS/")
@@ -26,3 +34,12 @@ while True:
 	print 'pseudo speed paramater is: ', vel, 'turning speed:', theta	
 	params = {'max_vel_x' : vel, 'max_vel_y' : vel,'min_vel_y':-vel,'min_vel_y':-vel,'max_trans_vel':vel,'max_rot_vel':theta}
 	config = client.update_configuration(params)
+	#update db with squeeze activation and value sent
+	cur.execute ("""
+   UPDATE Sensors
+   SET value=%s, status=%s
+   WHERE sensorId=%s
+""", (splitted[1], 'On', 1000))
+	db.commit()
+    
+    
